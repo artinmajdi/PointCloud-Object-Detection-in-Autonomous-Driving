996edb7fb519bcfd4406797e18247a4d9decbe94
artin <mohammadsmajdi@email.arizona.edu>
2022-08-01 12:19:35 -0700

    fixing the synchronization. refactoring

--- removingGroundMATLAB/V3_Plot_Synchronization_Data_modified.m
+++ removingGroundMATLAB/V3_Plot_Synchronization_Data_modified.m
@@ -1,142 +1,193 @@
-close all; clear all; clc; colmap = parula;
+% [Dir,dataset_name] = get_example_directory(2);
+% cd(Dir)
+
+close all; clear; clc; colmap = parula;
 
 write = false;
+mode = 'radar_lidar_fused'; % radar_lidar_fused 'lidar_w_wo_ground'
 
-for example = 1%:4
+
+for example = 2:4
 
     [Dir,dataset_name] = get_example_directory(example);
-    
     % Dir = uigetdir('U:\ROS\'); % Data Directory
     
-    radar_time = load([Dir '/Radar_time_tag.txt']); % Radar Time File
-    lidar_time = load([Dir '/Lidar_time_tag.txt']); % Lidar Time File
-    camera_time = load([Dir '/Camera_2_time_tag.txt']); %Camera Time File
+
+    [radar_time , lidar_time , camera_time] = load_time_values(Dir);
     
     if write == true
-        v=VideoWriter([Dir,'/',dataset_name,'_lidar_only.avi']);
-        v.FrameRate=2;
-        v.Quality=50;
-        open(v);
+        v = write_settings(mode, Dir, dataset_name);
     end
 
-    num_frames = 200; % size(radar_time,1)
 
-    for i = 1%:num_frames 
+    num_frames = 200; % size(radar_time,1)
+    for i = 1:num_frames         
+%         clc, 
+        disp(['Example:',num2str(example),'  Frame:(',num2str(i),'/',num2str(num_frames),')'])
         
-        % clc
-        % disp(['Example:',num2str(example),'  Frame:(',num2str(i),'/',num2str(num_frames),')'])
-        
-        try
-            visualize(Dir, i, radar_time, lidar_time , camera_time)
-%             visualize_lidar_only(Dir, i, radar_time, lidar_time , camera_time)
-            
-            drawnow();
+        try         
+            visualize(mode, Dir, i, radar_time, lidar_time , camera_time)
             
             if write == true
                 frame=getframe(gcf);
                 writeVideo(v,frame); 
             end
-
         catch
             continue
         end
         
     end
-%     close all;
-%     close(v)
+
+    close all;
+
+    if write == true
+        close(v)
+    end
 
 end
 
-function visualize(Dir, i, radar_time, lidar_time , camera_time)
-    
-    colmap = parula;
 
-    % Load Synchronized Data
+function [radar_time , lidar_time , camera_time] = load_time_values(Dir)
+
+    radar_time = load([Dir '/Radar_time_tag.txt']); % Radar Time File
+    lidar_time = load([Dir '/Lidar_time_tag.txt']); % Lidar Time File
+    camera_time = load([Dir '/Camera_2_time_tag.txt']); %Camera Time File
+
+end
+
+function v = write_settings(mode, Dir, dataset_name)
+
+    switch mode
+        case 'lidar_w_wo_ground'
+            post_tag = '_lidar_only';
+        
+        case 'radar_lidar_fused'
+            post_tag = '';
+    end
+
+    v=VideoWriter( [Dir, '/', dataset_name, post_tag, '.avi'] );
+    v.FrameRate=2;
+    v.Quality=50;
+    open(v);
+
+end
+
+function visualize(mode, Dir, i, radar_time, lidar_time , camera_time)
+
+    switch mode
+        case 'lidar_w_wo_ground'
+            visualize_lidar_w_wo_ground(Dir, i, radar_time, lidar_time , camera_time)
+        
+        case 'radar_lidar_fused'
+            visualize_radar_lidar_fused(Dir, i, radar_time, lidar_time , camera_time)
+    end
+
+    drawnow();
+
+end
+
+function visualize_radar_lidar_fused(Dir, i, radar_time, lidar_time , camera_time)
+    
+    % ---------------------------Load RADAR times------------------------------------------ %
     radar = load([Dir '\Radar\' num2str(radar_time(i,1)) '_.txt']); % Load Radar Frame
-    [diff,ind] = min(abs(lidar_time(:,2)-radar_time(i,2))); % Find Synchronized Lidar Frame
-    
-    lidar_r = load([Dir '\Lidar_r\' num2str(ind) '_.txt']); % Load Lidar Frame
-    [diff,ind] = min(abs(camera_time(:,2)-radar_time(i,2))); % Find Synchronized Camera Image
 
+
+    % ---------------------------Load Synchronized LIDAR w/wo background times ------------ %
+    [diff,ind] = min(abs(lidar_time(:,2)-radar_time(i,2))); % Find Synchronized Lidar Frame
+    lidar_r = load([Dir '\Lidar_r\' num2str(ind) '_.txt']); % Load Lidar Frame
+   
+
+    % ---------------------------Load Synchronized CAMERA times --------------------------- %
+    [diff,ind] = min(abs(camera_time(:,2)-radar_time(i,2))); % Find Synchronized Camera Image
     camera = imread([Dir '\Camera\' num2str(ind) '_.jpg']); % Load Camera Image
     
 
-    % Plot Syncronized Data
+
+    % ------------------------ Plot Syncronized Data  ------------------------------------- %    
     figure(1)
     set(gcf, 'Position', get(0, 'Screensize')); 
     set(gcf, 'InvertHardcopy', 'off')
 
-    % Birds Eye View Plot
-    subplot(2,4,[1,2,3,5,6,7])
-    scatter3(-radar(:,4),radar(:,3),radar(:,5),20,radar(:,9),'filled') % Plot Radar Data
-    set(gca,'Color',[colmap(1,:)]);grid on;
-    set(gca,'linewidth',2,'fontsize',15,'fontweight','bold');
 
-    hold on
-
-    scatter3(lidar_r(:,2),lidar_r(:,1),lidar_r(:,3),3,'w','filled') % Plot Lidar Data
-    hold off
-
-    view(2) % Set View to BirdsEye
-    c = colorbar;
-    xlim([-150 150]); ylim([0 300]); zlim([-50,50]); caxis([30 80]); % Plot Axis Limits
-    xlabel('X(m)');ylabel('Y(m)');zlabel('Z(m)');c.Label.String = 'RCS(dB)';
-
-    % Camera Image
-    subplot(2,4,[4,8])
-    imshow(camera); % Plot Camera Image
-    sgtitle([Dir '    Frame ' num2str(i-1)],'fontsize',15,'fontweight','bold','Interpreter', 'none')
+        % --------------------Plotting the RADAR & LIDAR points---------------------------- %
+        subplot(2,4,[1,2,3,5,6,7])
+        scatter3_plot(radar,   false, 'radar'),  hold on
+        scatter3_plot(lidar_r, true,  'lidar'),  hold off
+        
+        
+        % --------------------Plotting the CAMERA image------------------------------------ %
+        subplot(2,4,[4,8])
+        imshow(camera);
+        
+        % --------------------------------------------------------------------------------- %     
+        sgtitle([Dir '    Frame ' num2str(i-1)],'fontsize',15,'fontweight','bold','Interpreter', 'none')
 
 end
 
-function visualize_lidar_only(Dir, i, radar_time, lidar_time , camera_time)
+function visualize_lidar_w_wo_ground(Dir, i, radar_time, lidar_time , camera_time)
     
-    colmap = parula;
+    % Load Synchronized LIDAR w/wo background --------------------------------------- %
+    [diff,ind] = min(abs(lidar_time(:,2)-radar_time(i,2))); 
+    lidar   = load([Dir '\Lidar\'   num2str(ind) '_.txt']); 
+    lidar_r = load([Dir '\Lidar_r\' num2str(ind) '_.txt']); 
 
-    % Load Synchronized Data
-    % radar = load([Dir '\Radar\' num2str(radar_time(i,1)) '_.txt']); % Load Radar Frame
-    [diff,ind] = min(abs(lidar_time(:,2)-radar_time(i,2))); % Find Synchronized Lidar Frame
-    
-    lidar = load([Dir '\Lidar\' num2str(ind) '_.txt']); % Load Lidar Frame
+
+    % Load Synchronized CAMERA ------------------------------------------------------ %
     [diff,ind] = min(abs(camera_time(:,2)-radar_time(i,2))); % Find Synchronized Camera Image
-
-    lidar_r = load([Dir '\Lidar_r\' num2str(ind) '_.txt']); % Load Lidar Frame
-
     camera = imread([Dir '\Camera\' num2str(ind) '_.jpg']); % Load Camera Image
     
 
-    % Plot Syncronized Data
+    % ------------------------Plot Syncronized Data ----------------------------------- %    
     figure(1)
     set(gcf, 'Position', get(0, 'Screensize')); 
     set(gcf, 'InvertHardcopy', 'off')
 
-    subplot(131)
 
-    scatter3(-lidar(:,2),lidar(:,1),lidar(:,3),3,'w','filled') % Plot Lidar Data
+        % --------------------Plotting the LIDAR points---------------------------------- %
+        subplot(131)
+        scatter3_plot(lidar, true, 'lidar')
+
+        % --------------------Plotting the LIDAR WITHOUT BACKGROUND points--------------- %
+        subplot(132)
+        scatter3_plot(lidar_r, true, 'lidar')
+        
+        % --------------------Plotting the CAMERA image---------------------------------- %
+        subplot(133)
+        imshow(camera);
+    
+        % ------------------------------------------------------------------------------- %    
+        sgtitle([Dir '    Frame ' num2str(i-1)],'fontsize',15,'fontweight','bold','Interpreter', 'none')
+
+end
+
+function scatter3_plot(pointcloud, bird_view, mode)
+    switch mode
+        case 'radar'
+            scatter3(-pointcloud(:,4),pointcloud(:,3),pointcloud(:,5),20,pointcloud(:,9),'filled')
+
+        case 'lidar'
+            scatter3(-pointcloud(:,2),pointcloud(:,1),pointcloud(:,3),3,'w','filled') % Plot Lidar Data        
+            plotting_settings(bird_view)            
+    end
+end
+
+function plotting_settings(bird_view)
+
+    colmap = parula;
+
     set(gca,'Color',[colmap(1,:)]);grid on;
     set(gca,'linewidth',2,'fontsize',15,'fontweight','bold');
 
-    view(2) % Set View to BirdsEye
-    c = colorbar;
-    xlim([-150 150]); ylim([0 300]); zlim([-50,50]); caxis([30 80]); % Plot Axis Limits
+    % Set View to BirdsEye
+    if bird_view
+        view(2) 
+    end
+
+    xlim([-100 100]); ylim([0 200]); zlim([-50,50]); caxis([30 80]); % Plot Axis Limits
     xlabel('X(m)');ylabel('Y(m)');zlabel('Z(m)');
 
-    % Lidar with removed background Image
-    subplot(132)
-    scatter3(lidar_r(:,2),lidar_r(:,1),lidar_r(:,3),3,'w','filled') % Plot Lidar Data
-    set(gca,'Color',[colmap(1,:)]);grid on;
-    set(gca,'linewidth',2,'fontsize',15,'fontweight','bold');
-
-    view(2) % Set View to BirdsEye
     c = colorbar;
-    xlim([-150 150]); ylim([0 300]); zlim([-50,50]); caxis([30 80]); % Plot Axis Limits
-    xlabel('X(m)');c.Label.String = 'RCS(dB)';
-%     axis off
-
-    subplot(133)
-    imshow(camera); % Plot Camera Image
-
-    sgtitle([Dir '    Frame ' num2str(i-1)],'fontsize',15,'fontweight','bold','Interpreter', 'none')
+    c.Label.String = 'RCS(dB)';
 
 end
 
--- removingGroundMATLAB/lidar_ground_removal_loop.m
+++ removingGroundMATLAB/lidar_ground_removal_loop.m
@@ -1,11 +1,6 @@
 % lidar ground removal
 
-clc; clear all; close all;
-
-
-% Example 0
-% data_dir = 'Y:/RawData/20210622_Sys362_64N_1Tx_Angle_Finding_Raw_Data/20210622_Sys362_64N_1Tx_Angle_Finding_Raw_Data_LidarCam/Part_0';
-% test = '0El0Az_25m_2021-06-22-12-31-08';
+clc; clear; close all;
 
 
 num_frames = 200;
@@ -23,5 +18,43 @@ for example = 1:4
         file_dir_out = [folder_dir, '/Lidar_r/', num2str(i), '_.txt'];
         lidar_r = removing_lidar_ground(file_dir_in, file_dir_out);
     end
+
+end
+
+
+
+function lidar_r = removing_lidar_ground(file_dir_in, file_dir_out)
+
+    lidar = load(file_dir_in);
+    x = lidar(:, 1);
+    y = lidar(:, 2);
+    z = lidar(:, 3);
+    intensity = lidar(:, 4);
+    tm = lidar(:, 5);
+    ix = lidar(:, 6);
+    [x, y] = lidar_rotation(x, y);
+    
+    % camera = imread([folder_dir, '/Camera/', num2str(i), '_.jpg']);
+    
+    pc = pointCloud([y, x, z]);
+    groundPtsIdx = segmentGroundSMRF(pc);
+    pc_ground_removed = select(pc, ~groundPtsIdx);
+    xr = pc_ground_removed.Location(:, 1);
+    yr = pc_ground_removed.Location(:, 2);
+    zr = pc_ground_removed.Location(:, 3);
+    intensity_r = intensity(~groundPtsIdx);
+    tm_r = tm(~groundPtsIdx);
+    ix_r = ix(~groundPtsIdx);
+    
+    
+    % figure, subplot(121) %, imshow(camera)
+    % scatter3(xr, yr, zr, 1, 'filled', 'w');
+    % view([0, 90]), colorbar, caxis([0, 40])
+    
+
+    % save([folder_dir , '/' , num2str(i) , '_coordinates.mat'],'x','y','z','xr','yr','zr')
+    
+    lidar_r = [xr,-yr,zr,intensity_r,tm_r,ix_r];
+    writematrix(lidar_r , file_dir_out)
 
 end
